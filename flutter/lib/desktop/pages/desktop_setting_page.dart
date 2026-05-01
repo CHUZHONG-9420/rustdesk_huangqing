17a3869a1d3c9498f93ee76a586af0643f024728
CHUZHONG-9420 <262610750@qq.com>
2026-05-01 09:19:43 +0800

    260501_0919

--- flutter/lib/desktop/pages/desktop_setting_page.dart
+++ flutter/lib/desktop/pages/desktop_setting_page.dart
@@ -2485,8 +2485,8 @@ class _AboutState extends State<_About> {
                 ),
                 _buildLinkButton(
                   icon: Icons.code_rounded,
-                  label: '本软件源代码',
-                  url: 'https://github.com/CHUZHONG-9420/rustdesk_huangqing',
+                  label: '软件源代码',
+                  url: 'https://rustdesk.6w8.top/huangqing',
                   context: context,
                 ),
                 _buildLinkButton(
